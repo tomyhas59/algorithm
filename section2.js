403897c22102df249dc88f1304f60051737c9819
yh9035926 <yh9035926@naver.com>
2023-10-26 09:52:14 +0900

    let flag = 1;

--- section2.js
+++ section2.js
@@ -47,8 +47,8 @@ let A = [1, 3, 3, 1, 3];
 let B = [3, 1, 2, 2, 3];
 console.log(solution(A, B)); */
 
-//점수 계산
-function solution(s) {
+//점수 계산 연속으로 맞히면 점수 더 줌
+/* function solution(s) {
   let answer = 0,
     cnt = 0;
   for (let x of s) {
@@ -60,4 +60,90 @@ function solution(s) {
   return answer;
 }
 let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
+ */
+
+//등수 구하기 Array.from({ length: n }, () => 1);
+/* function solution(s) {
+  let n = s.length;
+  let answer = Array.from({ length: n }, () => 1);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (s[j] > s[i]) answer[i]++;
+    }
+  }
+  return answer;
+}
+let arr = [87, 89, 92, 100, 76];
+console.log(solution(arr)); */
+
+//격자판 최대합
+/* function solution(arr) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = arr.length;
+  let sum1 = 0,
+    sum2 = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum1 = sum2 = 0;
+    for (let j = 0; j < arr.length; j++) {
+      sum1 += arr[i][j];
+      sum2 += arr[j][i];
+    }
+    answer = Math.max(answer, sum1, sum2);
+  }
+  sum1 = sum2 = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum1 += arr[i][i];
+    sum2 += arr[i][n - i - 1];
+  }
+  answer = Math.max(answer, sum1, sum2);
+  return answer;
+}
+let arr = [
+  [110, 13, 10, 16, 16],
+  [112, 14, 74, 615, 61],
+  [13, 67, 547, 12, 12],
+  [14, 63, 15, 24, 34],
+  [15, 13, 86, 12, 11],
+];
+console.log(solution(arr));
+ */
+
+//봉우리
+function solution(arr) {
+  let answer = 0;
+  let n = arr.length;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let flag = 1;
+      for (let k = 0; k < 4; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+        if (
+          nx >= 0 &&
+          nx < n &&
+          ny >= 0 &&
+          ny < n &&
+          arr[nx][ny] >= arr[i][j]
+        ) {
+          flag = 0;
+          break;
+        }
+      }
+      if (flag) {
+        answer++;
+      }
+    }
+  }
+  return answer;
+}
+let arr = [
+  [5, 3, 7, 2, 3],
+  [3, 7, 1, 6, 1],
+  [7, 2, 5, 3, 4],
+  [4, 3, 6, 4, 1],
+  [8, 7, 3, 5, 2],
+];
 console.log(solution(arr));
