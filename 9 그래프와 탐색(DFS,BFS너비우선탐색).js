2be08edf3f70993c2ffe21dc7af6d4becf06efbe
yh9035926 <109195043+yh9035926@users.noreply.github.com>
2023-12-31 10:38:45 +0900

    back 할 때 pop

--- 9 그래프와 탐색(DFS,BFS너비우선탐색).js
+++ 9 그래프와 탐색(DFS,BFS너비우선탐색).js
@@ -1,1 +1,48 @@
-//그래프와 인접행렬
+//경로탐색 DFS 인접행렬 노드 개수가 적을 때
+
+function solution(arr, n) {
+  let answer = [];
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let graph = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+  let route = [];
+
+  for (let [a, b] of arr) {
+    graph[a][b] = 1;
+  }
+
+  console.log(graph);
+
+  function DFS(v) {
+    if (v === n) {
+      answer.push(route.slice());
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (graph[v][i] && ch[i] === 0) {
+          ch[i] = 1;
+          route.push(i);
+          DFS(i);
+          ch[i] = 0;
+          route.pop();
+        }
+      }
+    }
+  }
+  route.push(1);
+  //1부터 시작이니 1을 처음에 넣어놓기
+  ch[1] = 1;
+  DFS(1);
+  return answer;
+}
+
+let arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(solution(arr, 5));
