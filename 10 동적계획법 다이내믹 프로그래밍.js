490085ad82cca3547e17f1e99957850f0c23d5a6
yh9035926 <yh9035926@naver.com>
2024-01-05 06:26:41 +0900

    최대 부분 증가수열

--- 10 동적계획법 다이내믹 프로그래밍.js
+++ 10 동적계획법 다이내믹 프로그래밍.js
@@ -1,5 +1,5 @@
 //계단오르기 다이내믹(원리 파악 중요)
-function solution(n) {
+/* function solution(n) {
   if (n === 1) {
     return 1;
   }
@@ -17,4 +17,44 @@ function solution(n) {
 
   return dy[n];
 }
-console.log(solution(7));
+console.log(solution(7)); */
+
+//돌다리 건너기
+/* function solution(n) {
+ 
+  const dy = new Array(n + 1);
+  dy[0] = 1;
+  dy[1] = 1;
+  dy[2] = 2;
+
+  for (let i = 3; i <= n + 1; i++) {
+    dy[i] = dy[i - 3] + dy[i - 2] + dy[i - 1];
+  }
+
+  return dy[n + 1];
+}
+console.log(solution(7)); */
+
+//최대 부분 증가수열
+function solution(arr) {
+  let answer = 0;
+
+  let dy = Array.from({ length: arr.length }, () => 0);
+  dy[0] = 1;
+  for (let i = 0; i < arr.length; i++) {
+    let max = 0;
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[i] > arr[j] && dy[j] > max) {
+        max = dy[j];
+      }
+    }
+    dy[i] = max + 1;
+    answer = Math.max(answer, dy[i]);
+    console.log(dy);
+  }
+
+  return answer;
+}
+
+let arr = [5, 3, 7, 8, 6, 2, 9, 4];
+console.log(solution(arr));
