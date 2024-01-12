6d62e0d59b5e054358d97fcbc4c4a361ac65515b
yh9035926 <yh9035926@naver.com>
2024-01-13 05:21:19 +0900

    냅색 알고리즘

--- 10 동적계획법 다이내믹 프로그래밍.js
+++ 10 동적계획법 다이내믹 프로그래밍.js
@@ -36,7 +36,7 @@ console.log(solution(7)); */
 console.log(solution(7)); */
 
 //최대 부분 증가수열
-function solution(arr) {
+/* function solution(arr) {
   let answer = 0;
 
   let dy = Array.from({ length: arr.length }, () => 0);
@@ -57,4 +57,22 @@ function solution(arr) {
 }
 
 let arr = [5, 3, 7, 8, 6, 2, 9, 4];
-console.log(solution(arr));
+console.log(solution(arr)); */
+
+//동전교환 (냅색 알고리즘)
+
+function solution(m, coin) {
+  let answer = 0;
+  let dy = Array.from({ length: m + 1 }, () => 1000);
+  dy[0] = 0;
+
+  for (let i = 0; i < coin.length; i++) {
+    for (let j = coin[i]; j <= m; j++) {
+      dy[j] = Math.min(dy[j], dy[j - coin[i]] + 1);
+    }
+  }
+  answer = dy[m];
+  return answer;
+}
+let arr = [1, 2, 5];
+console.log(solution(15, arr));
