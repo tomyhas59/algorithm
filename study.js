9d9f6c47bc2e8f116dd86b82468df15fd2bc5bd3
yh9035926 <yh9035926@naver.com>
2024-01-19 04:16:41 +0900

    Map has set get

--- study.js
+++ study.js
@@ -1,5 +1,5 @@
 //가장 짧은 문자 거리
-function solution(s, t) {
+/* function solution(s, t) {
   let answer = [];
   let tArr = [];
 
@@ -20,3 +20,28 @@ function solution(s, t) {
 
 let arr = "teachermode";
 console.log(solution(arr, "e"));
+ */
+//문자열 압축 new Map 방법
+
+function solution(arr) {
+  let answer = "";
+  let charCountMap = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (charCountMap.has(arr[i])) {
+      charCountMap.set(arr[i], charCountMap.get(arr[i]) + 1);
+    } else {
+      charCountMap.set(arr[i], 1);
+    }
+  }
+
+  charCountMap.forEach((value, key) => {
+    answer += key + (value > 1 ? value : "");
+  });
+
+  return answer;
+}
+
+let arr = "KKHSSSSSSSE";
+
+console.log(solution(arr));
