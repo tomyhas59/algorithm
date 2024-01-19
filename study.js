c8d24b4de43e219caf85e40d85627763acbe541e
yh9035926 <109195043+yh9035926@users.noreply.github.com>
2024-01-20 01:24:28 +0900

    String(arr[i])

--- study.js
+++ study.js
@@ -22,7 +22,7 @@ let arr = "teachermode";
 console.log(solution(arr, "e"));
  */
 //문자열 압축 new Map 방법
-
+/* 
 function solution(arr) {
   let answer = "";
   let charCountMap = new Map();
@@ -43,5 +43,28 @@ function solution(arr) {
 }
 
 let arr = "KKHSSSSSSSE";
+
+console.log(solution(arr));
+ */
+
+//자리수의 합
+function solution(arr) {
+  let answer;
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let x of arr) {
+    let sum =
+      Math.floor(x / 100) + Math.floor((x % 100) / 10) + Math.floor(x % 10);
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (max === sum) {
+      if (x > answer) answer = x;
+    }
+  }
+  return answer;
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
 
 console.log(solution(arr));
