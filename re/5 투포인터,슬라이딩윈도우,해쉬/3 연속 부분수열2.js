44b6b07fd159a8de6260cefd07b592fee2d7e3b1
이용현 <yh9035926@naver.com>
2025-04-21 18:06:35 +0900

    최대매출

--- re/5 투포인터,슬라이딩윈도우,해쉬/3 연속 부분수열2.js
+++ re/5 투포인터,슬라이딩윈도우,해쉬/3 연속 부분수열2.js
@@ -18,3 +18,23 @@
  ▣ 출력예제 1
  10
  */
+
+function solution(arr, m) {
+  let answer = 0;
+  let lt = 0;
+  let sum = 0;
+
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+
+    while (sum > m) {
+      sum -= arr[lt++];
+    }
+
+    answer += rt - lt + 1;
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 3, 1, 2, 3], 5));
