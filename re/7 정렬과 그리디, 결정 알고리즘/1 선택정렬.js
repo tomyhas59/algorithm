73a1a57da68f0bb947e23ca14a2266c74eec3a97
이용현 <yh9035926@naver.com>
2025-05-12 16:09:40 +0900

    bubble

--- re/7 정렬과 그리디, 결정 알고리즘/1 선택정렬.js
+++ re/7 정렬과 그리디, 결정 알고리즘/1 선택정렬.js
@@ -4,3 +4,22 @@
 N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
 정렬하는 방법은 선택정렬입니다.
 */
+
+function solution(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let idx = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) {
+        idx = j;
+      }
+    }
+    if (idx !== i) {
+      [arr[i], arr[idx]] = [arr[idx], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+console.log(solution([13, 5, 11, 7, 23, 15]));
---
+++ re/7 정렬과 그리디, 결정 알고리즘/2  버블정렬.js
@@ -0,0 +1,19 @@
+/* 
+버블 정렬
+N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+정렬하는 방법은 버블정렬입니다.
+*/
+
+function solution(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(solution([13, 5, 11, 7, 23, 15]));
